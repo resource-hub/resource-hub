2165ae2233414ee9312bf4443ab791437ffe29e4
Ture <tu.cl@pm.me>
2019-09-07 22:02:54 +0200

    CD

---
+++ src/resource_hub/settings/demo.py
@@ -0,0 +1,14 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+
+ALLOWED_HOSTS = get_env_var('ALLOWED_HOSTS')
+
+# SMTP Server settings
+EMAIL_USE_TLS = get_env_var('EMAIL_USE_TLS')
+EMAIL_HOST = get_env_var('EMAIL_HOST')
+EMAIL_HOST_USER = get_env_var('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = get_env_var('EMAIL_HOST_PASSWORD')
+EMAIL_PORT = get_env_var('EMAIL_PORT')
+DEFAULT_FROM_EMAIL = get_env_var('DEFAULT_FROM_EMAIL')
