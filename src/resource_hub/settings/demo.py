5fbc3781017c5a42f43e34a265b39deb249d1085
Ture <tu.cl@pm.me>
2019-09-11 12:41:21 +0200

    CD

--- src/resource_hub/settings/demo.py
+++ src/resource_hub/settings/demo.py
@@ -2,6 +2,8 @@ from .base import *
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
 
 ALLOWED_HOSTS = get_env_var('ALLOWED_HOSTS')
 
