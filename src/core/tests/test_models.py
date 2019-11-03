f183db07b20403d1745fe1619fe267f28b287d2f
Ture <ture.cl.business@gmail.com>
2019-11-03 15:30:09 +0100

    update

--- src/core/tests/test_models.py
+++ src/core/tests/test_models.py
@@ -3,15 +3,15 @@ from django.contrib.auth.models import User
 from django.utils.dateparse import parse_date
 
 from core.models import *
-from . import create_test_users
+# from . import create_test_users
 
 
-class TestUser(TestCase):
-    @classmethod
-    def setUpTestData(cls):
-        create_test_users()
+# class TestUser(TestCase):
+#     @classmethod
+#     def setUpTestData(cls):
+#         create_test_users()
 
-    def test_person_first_name(self):
-        user = User.objects.get(username='testmate')
-        first_name = user.first_name
-        self.assertEqual('Test', first_name)
+#     def test_person_first_name(self):
+#         user = User.objects.get(username='testmate')
+#         first_name = user.first_name
+#         self.assertEqual('Test', first_name)
