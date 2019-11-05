706dcfcd1c57cc8c0eaf06a2e3060c6036fb4529
Ture <ture.cl.business@gmail.com>
2019-11-05 22:36:07 +0100

    rooms-init

--- src/rooms/urls.py
+++ src/rooms/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path, include
+from django.conf.urls import url
+import django.contrib.auth.views as auth
+
 from . import views
 
 app_name = 'rooms'
 urlpatterns = [
+    path('', views.index, name='index'),
 
 ]
