7988f81f48e9b56abe222cfd8dd4613342c9e20a
Ture <ture.cl.business@gmail.com>
2019-11-30 12:33:50 +0100

    test: Form manager

--- src/core/tests/test_forms.py
+++ src/core/tests/test_forms.py
@@ -1,31 +1,44 @@
 from datetime import datetime, timedelta
 import os
 
-from django.test import TestCase
 from django.forms import ValidationError
+from django.http.request import HttpRequest
+from django.test import TestCase
+
 from core.forms import *
 
 DATA = {
-    'username': 'peterpopper',
-    'email': 'test@ture.dev',
-    'password': 'we8dlz49opd',
-    'password1': 'we8dlz49opd',
-    'password2': 'we8dlz49opd',
-    'first_name': 'Peter',
-    'last_name': 'Popper',
-    'birth_date': '1997-05-31',
-    'old_email': 'test@test.de',
-    'new_email1': 'test2@test.de',
-    'new_email2': 'test2@test.de',
-    'account_holder': 'Peter Popper',
-    'iban': 'DE12500105170648489890',
-    'bic': 'INGDDEFFXXX',
+    "first_name": "Test",
+    "last_name": "Face",
+    "username": "test",
+    "email": "test@ture.dev",
+    "old_email": "test@ture.dev",
+    "new_email1": "test@mail.de",
+    "new_email2": "test@mail.de",
+    "password": "we8dlz49opd",
+    "password1": "we8dlz49opd",
+    "password2": "we8dlz49opd",
+    "birth_date": "1997-01-01",
+    "telephone_private": "123092813124",
+    "telephone_public": "00000000000",
+    "email_public": "test@mail.de",
+    "website": "https://test.de",
+    "info_text": "<h1>hello!</h1>",
+    "account_holder": "Test Face",
+    "iban": "DE12500105170648489890",
+    "bic": "INGDDEFFXXX",
+    "street": "Test Street",
+    "street_number": "12A",
+    "postal_code": "12345",
+    "city": "Test City",
+    "country": "Germany"
+
 }
 
 
 class TestUserBaseForm(TestCase):
     def setUp(self):
-        self.data = DATA
+        self.data = DATA.copy()
 
     def test_valid_data(self):
         form = UserBaseForm(data=self.data)
@@ -47,7 +60,7 @@ class TestUserBaseForm(TestCase):
 
 class TestBankAccountForm(TestCase):
     def setUp(self):
-        self.data = DATA
+        self.data = DATA.copy()
         self.path = os.path.dirname(os.path.abspath(__file__))
 
     def test_valid_data(self):
@@ -66,9 +79,30 @@ class TestBankAccountForm(TestCase):
                 self.assertFalse(form.is_valid())
 
 
+class TestInfoFormManager(TestCase):
+    def setUp(self):
+        self.data = DATA.copy()
+
+    def test_empty_form(self):
+        forms = InfoFormManager().get_forms()
+        self.assertTrue('info_form' in forms)
+        self.assertTrue('address_form' in forms)
+        self.assertTrue('bank_account_form' in forms)
+
+    def test_valid_data(self):
+        request = HttpRequest()
+        request.POST = self.data
+        info_form = InfoFormManager(request)
+        self.assertTrue(info_form.is_valid())
+
+        new_info = info_form.save()
+        info = Info.objects.get(pk=1)
+        self.assertEqual(new_info, info)
+
+
 class TestEmailChangeForm(TestCase):
     def setUp(self):
-        self.data = DATA
+        self.data = DATA.copy()
         self.user = User.objects.create_user(
             username=self.data['username'],
             email=self.data['old_email'],
