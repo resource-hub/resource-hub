d544c8b94887c1cf9a88238db1e6ade15fba317f
Ture <ture.cl.business@gmail.com>
2019-12-02 16:59:37 +0100

    token generator

--- src/core/tokens.py
+++ src/core/tokens.py
@@ -1,7 +1,9 @@
 from django.contrib.auth.tokens import PasswordResetTokenGenerator
-#from django.utils import six
 
 
 class TokenGenerator(PasswordResetTokenGenerator):
-    def test():
-        return
+    def _make_hash_value(self, user, timestamp):
+        return (
+            str(user.pk) + str(timestamp) +
+            str(user.is_active)
+        )
