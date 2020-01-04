46fa79864fd0066f8e98d8b2f77b688d3305e652
Ture <ture.cl.business@gmail.com>
2020-01-04 11:11:39 +0100

    refactoring: UI

--- src/rooms/hook_listeners.py
+++ src/rooms/hook_listeners.py
@@ -3,9 +3,9 @@ from django.template.loader import render_to_string
 
 def admin_sidebar(context, *args, **kwargs):
     return render_to_string(
-        template_name='rooms/hooks/admin_sidebar.html',
+        template_name='rooms/hooks/admin_sidebar.html', request=context.request,
     )
 
 
 def navigation_bar(context, *args, **kwargs):
-    return render_to_string(template_name='rooms/hooks/navigation_bar.html')
+    return render_to_string(template_name='rooms/hooks/navigation_bar.html', request=context.request)
