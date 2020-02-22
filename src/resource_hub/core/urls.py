0a2af8870f025a0baf27b37967abd45c06d1890c
Ture <ture.cl.business@gmail.com>
2020-02-22 11:48:35 +0100

    mv from urls to path

--- src/resource_hub/core/urls.py
+++ src/resource_hub/core/urls.py
@@ -1,6 +1,6 @@
 import django.contrib.auth.views as dj_auth
 from django.conf.urls import url
-from django.urls import include
+from django.urls import include, path
 from django.views.i18n import JavaScriptCatalog
 
 from resource_hub.api.urls import api_urls
@@ -12,90 +12,90 @@ JS_INFO_DICT = {
 }
 
 control_urls.register([
-    url(r'^$', control.Home.as_view(), name='home'),
-    url(r'^account/profile/(?P<scope>\w{0,50})/$',
-        control.AccountProfile.as_view(),
-        name='account_profile'),
-    url(r'^account/settings/(?P<scope>\w{0,50})/$',
-        control.AccountSettings.as_view(),
-        name='account_settings'),
+    path('', control.Home.as_view(), name='home'),
+    path('account/profile/<str:scope>/',
+         control.AccountProfile.as_view(),
+         name='account_profile'),
+    path('account/settings/<str:scope>/',
+         control.AccountSettings.as_view(),
+         name='account_settings'),
 
-    url(r'^notifications/$', control.Notifications.as_view(), name='notifications'),
+    path('notifications/', control.Notifications.as_view(), name='notifications'),
 
-    url(r'^finance/bank-accounts/$', control.FinanceBankAccounts.as_view(),
-        name='finance_bank_accounts'),
-    url(r'^finance/payment-methods/manage/$',
-        control.FinancePaymentMethodsManage.as_view(), name='finance_payment_methods_manage'),
-    url(r'^finance/payment-methods/manage/(?P<pk>\w{0,50})/$', control.FinancePaymentMethodsEdit.as_view(
+    path('finance/bank-accounts/', control.FinanceBankAccounts.as_view(),
+         name='finance_bank_accounts'),
+    path('finance/payment-methods/manage/',
+         control.FinancePaymentMethodsManage.as_view(), name='finance_payment_methods_manage'),
+    path('finance/payment-methods/manage/<int:pk>/', control.FinancePaymentMethodsEdit.as_view(
     ), name='finance_payment_methods_edit'),
-    url(r'^finance/payment-methods/add$',
-        control.FinancePaymentMethodsAdd.as_view(), name='finance_payment_methods_add'),
+    path('finance/payment-methods/add',
+         control.FinancePaymentMethodsAdd.as_view(), name='finance_payment_methods_add'),
 
-    url(r'^organizations/manage/$',
-        control.OrganizationsManage.as_view(),
-        name='organizations_manage'),
-    url(r'^organizations/create/$',
-        control.OrganizationsCreate.as_view(),
-        name='organizations_create'),
-    url(r'^organizations/manage/(?P<organization_id>\w{0,50})/profile/$',
-        control.OrganizationsProfile.as_view(),
-        name='organizations_profile'),
-    url(r'^organizations/manage/(?P<organization_id>\w{0,50})/profile/edit/(?P<scope>\w{0,50})/$',
-        control.OrganizationsProfileEdit.as_view(),
-        name='organizations_profile_edit'),
-    url(r'^organizations/manage/(?P<organization_id>\w{0,50})/members/$',
-        control.OrganizationsMembers.as_view(),
-        name='organizations_members'),
-    url(r'^organizations/manage/(?P<organization_id>\w{0,50})/members/add/$',
-        control.OrganizationsMembersAdd.as_view(),
-        name='organizations_members_add'),
+    path('organizations/manage/',
+         control.OrganizationsManage.as_view(),
+         name='organizations_manage'),
+    path('organizations/create/',
+         control.OrganizationsCreate.as_view(),
+         name='organizations_create'),
+    path('organizations/manage/<int:organization_id>/profile/',
+         control.OrganizationsProfile.as_view(),
+         name='organizations_profile'),
+    path('organizations/manage/<int:organization_id>/profile/edit/<str:scope>/',
+         control.OrganizationsProfileEdit.as_view(),
+         name='organizations_profile_edit'),
+    path('organizations/manage/<int:organization_id>/members/',
+         control.OrganizationsMembers.as_view(),
+         name='organizations_members'),
+    path('organizations/manage/<int:organization_id>/members/add/',
+         control.OrganizationsMembersAdd.as_view(),
+         name='organizations_members_add'),
 
-    url(r'^locations/create/$', control.LocationsCreate.as_view(),
-        name='locations_create'),
-    url(r'^locations/manage/$', control.LocationsManage.as_view(),
-        name='locations_manage'),
-    url(r'^locations/manage/(?P<location_id>\w{0,50})/profile/edit$', control.LocationsProfileEdit.as_view(),
-        name='locations_profile_edit'),
+    path('locations/create/', control.LocationsCreate.as_view(),
+         name='locations_create'),
+    path('locations/manage/', control.LocationsManage.as_view(),
+         name='locations_manage'),
+    path('locations/manage/<int:location_id>/profile/edit', control.LocationsProfileEdit.as_view(),
+         name='locations_profile_edit'),
 ])
 
 api_urls.register([
-    url(r'^users/search/$', api.UserSearch.as_view(), name='search_users'),
-    url(r'^actors/list$', api.ActorList.as_view(), name='actor_list'),
-    url(r'^actors/change$',
-        api.ActorChange.as_view(), name='actor_change'),
-    url(r'locations/search/$', api.Locations.as_view(),
-        name='locations_search')
+    path('users/search/', api.UserSearch.as_view(), name='search_users'),
+    path('actors/list', api.ActorList.as_view(), name='actor_list'),
+    path('actors/change',
+         api.ActorChange.as_view(), name='actor_change'),
+    path(r'locations/search/', api.Locations.as_view(),
+         name='locations_search')
 ])
 
 app_name = 'core'
 urlpatterns = [
-    url(r'^$', site.index, name='index'),
-    url(r'^home/$', site.Home.as_view(), name='home'),
-    url(r'^bug/$', site.ReportBug.as_view(), name='report_bug'),
-    url(r'^language/$', site.Language.as_view(), name='language'),
-    url(r'^terms/$', site.Terms.as_view(), name='terms'),
+    path('', site.index, name='index'),
+    path('home/', site.Home.as_view(), name='home'),
+    path('bug/', site.ReportBug.as_view(), name='report_bug'),
+    path('language/', site.Language.as_view(), name='language'),
+    path('terms/', site.Terms.as_view(), name='terms'),
 
-    url(r'^register/$', auth.Register.as_view(), name='register'),
-    url(r'^login/$', auth.custom_login, name='login'),
-    url(r'^logout/$', dj_auth.LogoutView.as_view(
+    path('register/', auth.Register.as_view(), name='register'),
+    path('login/', auth.custom_login, name='login'),
+    path('logout/', dj_auth.LogoutView.as_view(
         template_name='core/logout.html'), name='logout'),
     url(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
         auth.Activate.as_view(), name='activate'),
-    url(r'^password/reset/$', dj_auth.PasswordResetView.as_view(
+    path('password/reset/', dj_auth.PasswordResetView.as_view(
         template_name='core/password_reset.html', success_url='/password/reset/done/', email_template_name='core/mail_password_reset.html'), name='password_reset'),
-    url(r'^password/reset/done/$', dj_auth.PasswordResetDoneView.as_view(
+    path('password/reset/done/', dj_auth.PasswordResetDoneView.as_view(
         template_name='core/password_reset_done.html', ), name='password_reset_done'),
     url(r'^password/reset/confirm/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$', dj_auth.PasswordResetConfirmView.as_view(
         template_name='core/password_reset_confirm.html', success_url='/password/reset/complete/'), name='password_reset_confirm'),
-    url(r'^password/reset/complete/$', dj_auth.PasswordResetCompleteView.as_view(
+    path('password/reset/complete/', dj_auth.PasswordResetCompleteView.as_view(
         template_name='core/password_reset_complete.html')),
-    url(r'^actor/set$', auth.SetRole.as_view(), name='actor_set'),
+    path('actor/set', auth.SetRole.as_view(), name='actor_set'),
 
-    url(r'^i18n/', include('django.conf.urls.i18n')),
-    url(r'^jsi18n/$', JavaScriptCatalog.as_view(), JS_INFO_DICT),
-    url(r'^jsi18n.js$', JavaScriptCatalog.as_view(
+    path('i18n/', include('django.conf.urls.i18n')),
+    path('jsi18n/', JavaScriptCatalog.as_view(), JS_INFO_DICT),
+    path('jsi18n.js', JavaScriptCatalog.as_view(
         packages=['recurrence']), name='jsi18n'),
 
-    url(r'^locations/(?P<location_id>\w{0,50})/$',
-        site.LocationsProfile.as_view(), name='locations_profile'),
+    path('locations/<int:location_id>/',
+         site.LocationsProfile.as_view(), name='locations_profile'),
 ]
--- src/resource_hub/venues/urls.py
+++ src/resource_hub/venues/urls.py
@@ -7,23 +7,23 @@ from resource_hub.venues import api, views
 app_name = 'venues'
 
 api_urls.register([
-    url(r'^venues/$', api.Venues.as_view(), name='venues'),
-    url(r'^venues/(?P<venue_id>\w{0,50})/events/$',
-        api.VenueEvents.as_view(), name='venues_event_feed'),
+    path('venues/', api.Venues.as_view(), name='venues'),
+    path('venues/<int:venue_id>/events/',
+         api.VenueEvents.as_view(), name='venues_event_feed'),
 ])
 
 control_urls.register([
-    url(r'^venues/manage/$', views.VenuesManage.as_view(), name='venues_manage'),
-    url(r'^venues/manage/(?P<venue_id>\w{0,50})/profile/edit/$',
-        views.VenuesProfileEdit.as_view(), name='venues_profile_edit'),
-    url(r'^venues/create/$', views.VenuesCreate.as_view(), name='venues_create'),
+    path('venues/manage/', views.VenuesManage.as_view(), name='venues_manage'),
+    path('venues/manage/<int:venue_id>/profile/edit/',
+         views.VenuesProfileEdit.as_view(), name='venues_profile_edit'),
+    path('venues/create/', views.VenuesCreate.as_view(), name='venues_create'),
 ])
 
 urlpatterns = [
     path('', views.index, name='index'),
-    url(r'^(?P<venue_id>\w{0,50})/details$',
-        views.VenueDetails.as_view(), name='venue_details'),
-    url(r'^(?P<venue_id>\w{0,50})/events/create/$',
-        views.VenueEventsCreate.as_view(), name='events_create'),
+    path('<int:venue_id>/details',
+         views.VenueDetails.as_view(), name='venue_details'),
+    path('<int:venue_id>/events/create/',
+         views.VenueEventsCreate.as_view(), name='events_create'),
 
 ]
