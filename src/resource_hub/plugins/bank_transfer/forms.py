7f343570b636b933dd4a6c641e3506ac3a77f0e0
Ture <ture.cl.business@gmail.com>
2020-03-11 17:58:55 +0100

    refactor: remove old form

--- src/resource_hub/plugins/bank_transfer/forms.py
+++ src/resource_hub/plugins/bank_transfer/forms.py
@@ -8,41 +8,6 @@ from resource_hub.core.models import BankAccount
 
 from .models import BankTransfer
 
-# from resource_hub.core.forms import BankAccountForm
-
-
-# class BankAccountForm(forms.Form):
-#     account_holder = forms.CharField(max_length=100)
-#     iban = forms.CharField(max_length=50)
-#     bic = forms.CharField(max_length=10)
-
-#     def clean_iban(self):
-#         iban = self.cleaned_data['iban']
-#         if iban:
-#             try:
-#                 IBAN(iban)
-#             except ValueError as e:
-#                 raise forms.ValidationError(
-#                     _('Invalid IBAN: ') + str(e), code='invalid-iban')
-#         return iban
-
-#     def clean_bic(self):
-#         bic = self.cleaned_data['bic']
-#         if bic:
-#             try:
-#                 BIC(bic)
-#             except ValueError as e:
-#                 raise forms.ValidationError(
-#                     _('Invalid BIC: ') + str(e), code='invalid-bic')
-#         return bic
-
-#     def save(self, commit=True):
-#         new_bank_account = BankAccount.objects.create(
-#             account_holder=self.cleaned_data['account_holder'],
-#             iban=self.cleaned_data['iban'],
-#             bic=self.cleaned_data['bic'],
-#         )
-
 
 class BankTransferForm(BankAccountForm):
     # hacky way to make multi model form inheritance work
