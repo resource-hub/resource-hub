5f5cdea47555f284792c945c4f1219b4f04e0bd8
Ture <ture.cl.business@gmail.com>
2020-03-12 13:18:08 +0100

    fix dark mode problems

--- src/resource_hub/venues/models.py
+++ src/resource_hub/venues/models.py
@@ -49,8 +49,6 @@ class Venue(models.Model):
         on_delete=models.CASCADE,
     )
     thumbnail_original = models.ImageField(
-        null=False,
-        blank=True,
         upload_to='images/',
     )
     thumbnail = ImageSpecField(
