e538fdd36987e21ae08b71326ea2358e518944c9
Ture <ture.cl.business@gmail.com>
2020-03-27 11:46:48 +0100

    new summernote dep

--- src/resource_hub/core/fields.py
+++ src/resource_hub/core/fields.py
@@ -3,42 +3,17 @@ from django import forms
 from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db.models import DecimalField
 
-from django_summernote.widgets import SummernoteWidget
-
-#
-# form fields
-#
-ALLOWED_TAGS = [
-    'a', 'div', 'p', 'span', 'img', 'em', 'i', 'li', 'ol', 'ul', 'strong', 'br',
-    'h1', 'h2', 'h3', 'h4', 'h5', 'h6',
-    'table', 'tbody', 'thead', 'tr', 'td',
-    'abbr', 'acronym', 'b', 'blockquote', 'code', 'strike', 'u', 'sup', 'sub',
-]
-
-STYLES = [
-    'background-color', 'font-size', 'line-height', 'color', 'font-family'
-]
-
-ATTRIBUTES = {
-    '*': ['style', 'align', 'title', ],
-    'a': ['href', ],
-}
+from django_summernote.fields import SummernoteTextFormField
 
 
-class HTMLField(forms.CharField):
-    def __init__(self, *args, **kwargs):
-        super(HTMLField, self).__init__(*args, **kwargs)
-        self.widget = SummernoteWidget()
-
-    def to_python(self, value):
-        value = super(HTMLField, self).to_python(value)
-        return bleach.clean(
-            value, tags=ALLOWED_TAGS, attributes=ATTRIBUTES, styles=STYLES)
-
+class HTMLField(SummernoteTextFormField):
+    pass
 
 #
 # model fields
 #
+
+
 class PercentField(DecimalField):
     def __init__(self, *args, **kwargs):
         kwargs['default'] = 0
