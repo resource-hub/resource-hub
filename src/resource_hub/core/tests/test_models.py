5f47327231caa9e15a1af3a14b9e8eac18616a0b
Ture <ture.cl.business@gmail.com>
2020-04-09 15:43:25 +0200

    contract state tests

--- src/resource_hub/core/tests/test_models.py
+++ src/resource_hub/core/tests/test_models.py
@@ -1,8 +1,10 @@
 from django.test import TestCase
-from django.contrib.auth.models import User
-from django.utils.dateparse import parse_date
 
-from resource_hub.core.models import *
+from resource_hub.core.models import Contract
+
+# from django.contrib.auth.models import User
+# from django.utils.dateparse import parse_date
+
 # from . import create_test_users
 
 
@@ -15,3 +17,21 @@ from resource_hub.core.models import *
 #         user = User.objects.get(username='testmate')
 #         first_name = user.first_name
 #         self.assertEqual('Test', first_name)
+
+class TestContract(TestCase):
+    def setUp(self):
+        self.contract = Contract()
+
+    def test_illegal_moves(self):
+        state = Contract.STATE
+        moves = [
+            (state.INIT, state.FINALIZED),
+            (state.INIT, state.RUNNING),
+            (state.RUNNING, state.CANCELED),
+        ]
+        for edge in moves:
+            self.contract.state = edge[0]
+            self.assertRaises(
+                ValueError,
+                self.contract.move_to, edge[1]
+            )
