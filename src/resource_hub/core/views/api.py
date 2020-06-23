4f60e3fba1d212d98a0fefd3f907875563a35dbd
Ture <ture.cl.business@gmail.com>
2020-06-23 12:57:11 +0200

    deployment fixes

--- src/resource_hub/core/views/api.py
+++ src/resource_hub/core/views/api.py
@@ -27,7 +27,7 @@ class SmallResultsSetPagination(PageNumberPagination):
 
 
 class LargeResultsSetPagination(PageNumberPagination):
-    page_size = 4
+    page_size = 12
     page_size_query_param = 'page_size'
     max_page_size = 1000
 
